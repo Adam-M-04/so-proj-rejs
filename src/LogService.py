765786d4a8cf0294ff97ba180388b4d4293bc571
Adam <62599990+Adam-M-04@users.noreply.github.com>
2024-12-29 14:25:09 +0100

    Fill lacking docs

--- src/LogService.py
+++ src/LogService.py
@@ -5,6 +5,11 @@ import src.globals as GLOBALS
 
 class LogService:
     def __init__(self, log_dir='logs'):
+        """
+        Initializes the LogService with a given directory for log files.
+
+        :param log_dir: Directory where log files will be stored.
+        """
         self.log_dir = log_dir
         os.makedirs(self.log_dir, exist_ok=True)
         self.log_file = os.path.join(self.log_dir, f"log_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
@@ -12,18 +17,39 @@ class LogService:
         self._create_log_file()
 
     def _create_log_file(self):
-        self.file.write(f"Log file created on {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        """
+        Creates a new log file and writes the creation timestamp.
+        """
+        self.file.write(f"Logi symulacji utworzone {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
 
     def log(self, event):
+        """
+        Logs an event with a timestamp.
+
+        :param event: The event message to log.
+        """
         self.file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {event}\n")
         self.file.flush()
 
     def close(self):
+        """
+        Closes the log file.
+        """
         self.file.close()
 
 class BaseLogger:
     def __init__(self, prefix: str):
+        """
+        Initializes the BaseLogger with a given prefix.
+
+        :param prefix: Prefix to be used in log messages.
+        """
         self.prefix = prefix
 
     def log(self, event):
+        """
+        Logs an event with the specified prefix.
+
+        :param event: The event message to log.
+        """
         GLOBALS.logger.log(f"[{self.prefix}] {event}")
