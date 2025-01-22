dcb08e7fdc6c333e7019017b4ecf8d341135a777
Adam <62599990+Adam-M-04@users.noreply.github.com>
2025-01-22 17:56:39 +0100

    duplicated code

--- src/objects/passenger.py
+++ src/objects/passenger.py
@@ -28,14 +28,3 @@ def passenger(passenger_id, passengers_in_port, bridge_semaphore, passengers_on_
         LogService.log_static(f"Pasażer {passenger_id} wchodzi na mostek", logger_queue)
         passengers_in_port.remove(passenger_id)
         passengers_on_bridge.put(passenger_id)
-    while passenger_id not in passengers_after_trip and not bridge_close.is_set():
-        if not boarding_allowed.value or passenger_id not in passengers_in_port:
-            sleep(1)
-            continue
-        bridge_semaphore.acquire()
-        if not boarding_allowed.value or bridge_close.is_set():
-            bridge_semaphore.release()
-            continue
-        LogService.log_static(f"Pasażer {passenger_id} wchodzi na mostek", logger_queue)
-        passengers_in_port.remove(passenger_id)
-        passengers_on_bridge.put(passenger_id)
